3e2b0c97b606af4d4453360019506c9b5fef895e
johnwdjiang <johnwdjiang@tencent.com>
2020-12-07 11:30:35 +0800

    init test

---
+++ my_test/test_first.cpp
@@ -0,0 +1,26 @@
+//
+// Created by david on 2020/12/7.
+//
+#include "../include/leveldb/db.h"
+#include <string.h>
+#include <assert.h>
+#include <iostream>
+int main(){
+  leveldb::DB* db;
+  leveldb::Options options;
+  options.create_if_missing = true;
+  leveldb::Status status = leveldb::DB::Open(options,"/tmp/test", &db);
+  assert(status.ok());
+
+  std::string key="vic";
+  std::string value = "666";
+
+  status = db->Put(leveldb::WriteOptions(), key,value);
+  assert(status.ok());
+
+  status = db->Get(leveldb::ReadOptions(), key, &value);
+  assert(status.ok());
+  std::cout << key <<"'s value is:" << value << std::endl;
+  delete db;
+  return 0;
+}
